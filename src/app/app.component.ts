4f3aa325690306d4423c8902ecfbc0f684b603b2
Wayne Rumble <rumblewayne@gmail.com>
2018-01-25 13:41:31 +0000

    Adding directions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,9 +20,13 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {}
 
-  openMap() {
+  closeModal() {
+    this.modalActions.emit({action:"modal",params:['close']});
+  }
+
+  openChurchMap() {
     var mapProp = {
-          center: new google.maps.LatLng(51.0207156, 0.260662),
+          center: new google.maps.LatLng(51.020624, 0.2604413),
           zoom: 14,
           mapTypeId: google.maps.MapTypeId.ROADMAP,
           mapTypeControl: true,
@@ -31,15 +35,52 @@ export class AppComponent implements OnInit {
             mapTypeIds: ['roadmap', 'terrain']
           }
       };
-    var map = new google.maps.Map(document.getElementById("map"), mapProp);
-    let myLatlng = new google.maps.LatLng(51.0207156, 0.260662);
-    let marker = new google.maps.Marker({
+    var map = new google.maps.Map(document.getElementById("church-map"), mapProp);
+    let churchLL = new google.maps.LatLng(51.020624, 0.2604413);
+    let churchMarker = new google.maps.Marker({
            draggable: true,
            animation: google.maps.Animation.DROP,
-             position: myLatlng,
+             position: churchLL,
              icon: "",
              label: "St Dunstan's Church",
              map: map,
+    });
+    let farmLL = new google.maps.LatLng(51.1728154, 0.2218346);
+    let farmMarker = new google.maps.Marker({
+           draggable: true,
+           animation: google.maps.Animation.DROP,
+             position: farmLL,
+             icon: "",
+             label: "Juddwood Farm",
+             map: map,
+    });
+  }
+
+  openDirectionsMap() {
+        var directionsDisplay = new google.maps.DirectionsRenderer();
+        var mapCenter = new google.maps.LatLng(51.0965176, 0.1021581);
+        var mapOptions =
+                {
+                    zoom: 11,
+                    center: mapCenter,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP,
+                };
+    var map = new google.maps.Map(document.getElementById("directions-map"), mapOptions);
+    let center = new google.maps.LatLng(51.0207156, 0.260662);
+    directionsDisplay.setMap(map);
+
+    var directionsService = new google.maps.DirectionsService();
+    var start = new google.maps.LatLng(51.0965176, 0.1021581);
+    var end = new google.maps.LatLng(51.020624, 0.2604413);
+    var request = {
+        origin: start,
+        destination: end,
+        travelMode: google.maps.TravelMode.DRIVING
+    };
+    directionsService.route(request, function(response, status) {
+      if (status == google.maps.DirectionsStatus.OK) {
+        directionsDisplay.setDirections(response);
+      }
     });
   }
 }
